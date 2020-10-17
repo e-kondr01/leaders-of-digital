5d87aa6505972ad81ec119b982d34a516e843a96
e-kondr01 <e.kondr01@gmail.com>
2020-10-17 22:42:07 +0300

    heroku?

--- common/migrations/0009_auto_20201017_2103.py
+++ common/migrations/0009_auto_20201017_2103.py
@@ -16,5 +16,5 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='reports', to='common.Order')),
-            ],
+            ],)
     ]
